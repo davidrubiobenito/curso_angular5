a6360642d7f9d23c6080f60248341ab35ee036d8
David <davidrubiobenito@gmail.com>
2018-03-17 16:31:48 +0100

    Primer commit, proyecto spring boot

---
+++ src/main/java/com/drbotro/cursoAngular5/CursoAngular5Application.java
@@ -0,0 +1,12 @@
+package com.drbotro.cursoAngular5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoAngular5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoAngular5Application.class, args);
+	}
+}
